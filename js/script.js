33da349227833211050761ecdc8bca57da69bed9
Melinh <melinh@Melinhs-MacBook-Pro.local>
2018-10-09 17:46:51 -0500

    nav bar

--- js/script.js
+++ js/script.js
@@ -93,6 +93,35 @@ $(document).ready(function () {
 
    });
 
+   $("#navigation li a").click(function(e){
+    e.preventDefault();
+
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;
+    $("html, body").animation({ scrollTop: targetPosition - 50 }, "slow");
+   });
+
+
+
+   const nav = $("#navigation");
+   const navTop = nav.offset().top;
+
+   $(window).on("scroll", stickyNavigation);
+
+   function stickyNavigation() {
+
+    const body = $("body");
+
+    if ($(window).scrollTop() >= navTop) {
+        body.css("padding-top", nav.outerHeight() + "px");
+        body.addClass("fixedNav");
+    }
+    else{
+        body.css("padding-top", 0);
+        body.removeClass("fixedNav");
+    }
+   }
+
 });
 
 
